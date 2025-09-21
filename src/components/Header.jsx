85569a8211b373f1d4425818490223ba1fa5d9f5
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-21 14:06:41 +0200

    [upd] created Header component, exported it, and imported it into App.jsx, added css

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return <header className="container-fluid">
+	<div className="row">
+		<div className="col-12"><h1>Header</h1>
+			</div></div></header>
+};
+
+export default Header;
