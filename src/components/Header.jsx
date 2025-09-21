dbeeaeb8ccaea4675b4a233900c63504862fda52
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-21 15:50:33 +0200

    [upd] added logo, css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
 const Header = () => {
-  return <header className="container-fluid">
-	<div className="row">
-		<div className="col-12"><h1>Booflix</h1>
-			</div></div></header>
+  return (
+    <header className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <div className="d-flex align-items-center">
+            <img src="./imgs/booflix.jpg" />
+            <h1>
+              Booflix | <span> La Tua Alternativa al Cinema</span>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="container">
+    <div className="container my-5">
       <div className="row">
         <div className="col-12">
           <h1>Booflix</h1>
@@ -10,6 +10,7 @@ const HomePage = () => {
             <i>Film per veri appassionati</i>
           </h2>
         </div>
+		<div className="col-12">Elenco Film</div>
       </div>
     </div>
   );
