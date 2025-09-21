8914693d0b991cdf2e697230184719cf05819959
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-21 16:51:26 +0200

    [upd] added movie card and css, added sample image

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const HomePage = () => {
   return (
     <div className="container my-5">
       <div className="row">
-        <div className="col-12">
+        <div className="col-12 text-center">
           <h1>Booflix</h1>
           <h2>
-            <i>Film per veri appassionati</i>
+            <span>Film per veri appassionati</span>
           </h2>
         </div>
-		<div className="col-12">Elenco Film</div>
+      </div>
+      <div className="row gy-3">
+        <div className="col-12 col-md-6 col-lg-4">
+          <div className="card-movie">
+            <img
+              src="./imgs/movie.jpg"
+              className="cover-movie-card"
+              alt="Movie"
+            />
+            <div className="overlay">
+              <h2>Titolo Movie</h2>
+              <p>Autore</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
