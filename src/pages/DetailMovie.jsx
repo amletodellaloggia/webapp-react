22df073be3efbfa01d409c42a0e4fe8123a08aa6
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-22 12:44:18 +0200

    [upd] imported link to show button

--- src/pages/DetailMovie.jsx
+++ src/pages/DetailMovie.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const DetailMovie = () => {
   const { id } = useParams();
