de867ed33fa468ad5bf7ca6554bf6b024de5e863
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-25 13:44:46 +0200

    [upd] added values to Provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import DetailMovie from "./pages/DetailMovie";
 import NotFound from "./pages/NotFound";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalContext from "./contexts/globalContext";
+import { useState } from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <GlobalContext.Provider>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies/:id" element={<DetailMovie />} />
-        </Route>
+    <GlobalContext.Provider value={{ isLoading, setIsLoading }}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies/:id" element={<DetailMovie />} />
+          </Route>
           <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
     </GlobalContext.Provider>
   );
 }
